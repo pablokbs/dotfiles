b615094bc5dff9741f8dbf946e6dd1b280fbbd03
Pablo Fredrikson <pablo.kerberos@gmail.com>
2014-05-05 21:57:07 -0300

    switched to awesome

--- bin/baraction.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#conky | dzen2 -h 12 -ta r -x 600 -fn "-*-terminus-medium-*-*-*-12-*-*-*-*-*-*-*"
-conky | dzen2 -h 12 -ta r -x 550 -fn "-*-profont-medium-*-*-*-10-*-*-*-*-*-*-*"
-
-exit 0
